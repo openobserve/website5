937b921c94d40002c0b9c8c0eba780e79c508abd
ktx-monil <monil.desai@kiara.tech>
2025-01-17 16:24:39 +0530

    feat(core): Build CustomSection Component

---
+++ src/components/core/CustomSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="[defualtClasses, props.class || '']">
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  class: String,
+});
+const defualtClasses = computed(() => {
+  return ["container mx-auto px-10 md:py-10 md:mx-10", props.class].join("");
+});
+</script>
