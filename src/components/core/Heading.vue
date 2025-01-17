1dd46259ced6a19a99c7f57b8bc80b5da6738566
ktx-riya <riya.kukadiya@kiara.tech>
2025-01-17 14:09:21 +0530

    (feat/component):make title component

---
+++ src/components/core/Heading.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="[aligns[align]]">
+    <h1
+      class="text-2xl md:text-4xl font-semibold text-white"
+      v-html="title"
+    ></h1>
+    <p
+      v-if="description"
+      class="font-normal md:font-menium text-white text-base md:text-lg mt-5"
+      v-html="description"
+    ></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Heading",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    align: {
+      type: String,
+      default: "CENTER",
+      validator: (value) =>
+        ["LEFT", "CENTER", "RIGHT"].includes(value),
+    },
+  },
+  computed: {
+    aligns() {
+      return {
+        LEFT: "text-left",
+        CENTER: "text-center",
+        RIGHT: "text-right",
+      };
+    },
+  },
+};
+</script>
+
