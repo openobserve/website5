af48dfb4598278f98db8f4b9cf8cb5bed6cf0172
ktx-riya <riya.kukadiya@kiara.tech>
2025-01-21 17:49:19 +0530

    feat(component):build footer component, textWithGradient component, and blog with image component.

---
+++ src/components/HeaderComponents/TextWithGradient.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: "TextGradient",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    textGradientColor: {
+      type: String,
+      required: true,
+    },
+    textSize: {
+      type: String,
+      required: true,
+    },
+    align: {
+      type: String,
+      default: "left",
+      validator: (value) => ["left", "center", "right"].includes(value),
+    },
+  },
+  computed: {
+    computedClass() {
+      return `font-semibold text-${this.textSize} text-${this.align} ${this.textGradientColor} text-gradient`;
+    }
+  },
+};
+</script>
+
+<template>
+  <h1 :class="computedClass">
+    {{ title }}
+  </h1>
+</template>
+
+<style>
+.text-gradient {
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+</style>
