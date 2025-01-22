1e50a766c70fc0557ef420c67960ef03d178078d
ktx-monil <monil.desai@kiara.tech>
2025-01-22 20:27:11 +0530

    fix(component): Add CustomSection on each component and make required changes in customsection

--- src/components/core/CustomSection.vue
+++ src/components/core/CustomSection.vue
@@ -1,15 +1,25 @@
 <template>
-  <div :class="[defualtClasses, props.sectionClass || '']">
+  <div :class="[defaultClasses, props.sectionClass || '']">
     <slot></slot>
   </div>
 </template>
+
 <script setup>
 import { computed } from "vue";
 
 const props = defineProps({
-  sectionClass: String,
+  sectionClass: {
+    type: String,
+    default: "",
+  },
 });
-const defualtClasses = computed(() => {
-  return ["container mx-auto px-4 md:px-10 md:py-10", props.sectionClass].join("");
+
+const defaultClasses = computed(() => {
+  return [
+    "container mx-auto px-4 sm:px-6 lg:px-8 xl:px-12", // Handle horizontal padding on different screen sizes
+    "py-6 sm:py-8 md:py-12 lg:py-16 xl:py-20", // Handle vertical padding for each breakpoint
+    "w-full flex flex-col justify-start", // General layout setup
+    props.sectionClass,
+  ].join(" ");
 });
 </script>
