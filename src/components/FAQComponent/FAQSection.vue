bce4bee9bcda4dd4c8432a87c1f5e66cd039f588
ktx-riya <riya.kukadiya@kiara.tech>
2025-01-24 11:01:07 +0530

    fix(component):work on fix faq and blogcard css.

--- src/components/FAQComponent/FAQSection.vue
+++ src/components/FAQComponent/FAQSection.vue
@@ -18,25 +18,42 @@ const toggleAnswer = (index) => {
 
 <template>
   <div class="w-full bg-[url('/faqBg2.svg')] bg-cover bg-center bg-no-repeat h-full">
-      <!-- FAQ Items -->
-      <div class="space-y-4 mt-10 mb-10">
-        <div v-for="(item, index) in faqItems" :key="index">
+    <!-- FAQ Items -->
+    <div class="space-y-4 mt-10 mb-10">
+      <div v-for="(item, index) in faqItems" :key="index">
+        <div
+          class="flex justify-between items-center cursor-pointer p-4 group relative border border-transparent rounded-xl hover:border hover:border-gradient duration-300 transition-all"
+          @click="toggleAnswer(index)"
+        >
           <div
-            class="flex justify-between items-center cursor-pointer p-4 group relative border-0 border-transparent rounded-2xl hover:border-gradient-gray duration-300 transition-all"
-            @click="toggleAnswer(index)"
+            class="absolute inset-0 rounded-md bg-gradient-to-b from-[#296EA7] via-transparent to-[#000000] opacity-0 group-hover:opacity-20 transition-opacity duration-300 pointer-events-none"
+          ></div>
+          <h3
+            class="lg:font-medium lg:text-lg md:font-semibold text-base text-white mt-4 z-10"
           >
-          <div class="absolute inset-0 rounded-md bg-gradient-to-b from-[#296EA7] via-transparent to-[#000000] opacity-0 group-hover:opacity-20 transition-opacity duration-300 pointer-events-none"></div>
-            <h3 class="lg:font-medium lg:text-lg md:font-semibold text-base text-white mt-4 z-10">
-              {{ item.question }}
-            </h3>
-            <img 
-              src="/faq+Icon.svg" 
-              alt="toggle"
-              class="transition-transform duration-300 ease-in-out"
-              :class="{ 'rotate-45': visibleAnswers[index] }"
-            />
-          </div>
+            {{ item.question }}
+          </h3>
+          <img
+            src="/faq+Icon.svg"
+            alt="toggle"
+            class="transition-transform duration-300 ease-in-out"
+            :class="{ 'rotate-45': visibleAnswers[index] }"
+          />
+        </div>
 
+        <div
+          class="transition-all duration-300 ease-in-out overflow-hidden"
+          :class="{
+            'max-h-0 opacity-0': !visibleAnswers[index],
+            'max-h-96 opacity-100 mt-4': visibleAnswers[index],
+          }"
+        >
+          <p class="text-white lg:font-light text-sm md:text-base">
+            {{ item.answer }}
+          </p>
+        </div>
+      </div>
+    </div>
         <div
           class="transition-all duration-300 ease-in-out overflow-hidden"
           :class="{
@@ -59,11 +76,8 @@ const toggleAnswer = (index) => {
 }
 </style>
 
-
 <style scoped>
 .border-gradient {
-  border-image: linear-gradient(to bottom, #296EA7, transparent, #000000) 1;
-  border-image-slice: 1;
-  border-radius:1rem;
+  border-radius: 20px
 }
 </style>
