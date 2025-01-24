23adf8759055b2756c9a0f76808912eaab0782bf
ktx-riya <riya.kukadiya@kiara.tech>
2025-01-24 18:24:46 +0530

    fix(component):rebuild the separate core component for feature card and useCase card. and build its wrapper component.

--- src/components/cardComponent/UsecaseCardWithIconWrapper.vue
+++ src/components/cardComponent/UsecaseCardWithIconWrapper.vue
@@ -9,33 +9,6 @@ import CustomSectionBackground from "../core/CustomSectionBackground.vue";
 const props = defineProps({
   cardData: {
     type: Object,
-    required: true,
-  },
-});
-
-</script>
-
-<template>
-
-  <CustomSection>
-    <div class="flex flex-col max-w-full">
-      <Heading :title="props.cardData.title" :description="props.cardData.description" />
-      <div class="mt-10">
-        <div class="flex flex-col justify-center">
-          <IconTitleCard :cards="props.cardData.items" />
-        </div>
-      </div>
-    </div>
-  </CustomSection>
-</template><script setup>
-import { defineProps } from "vue";
-import IconTitleCard from "./UsecaseCardWithIcon.vue";
-import CustomSection from "../core/CustomSection.vue";
-import Heading from "../core/Heading.vue";
-import CustomSectionBackground from "../core/CustomSectionBackground.vue";
-
-
-const props = defineProps({
   cardData: {
     type: Object,
     required: true,
