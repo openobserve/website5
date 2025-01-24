10b1ae5490bbb968869cf65077ce4010f1b51f9a
krupa <krupa.jivani@kiara.tech>
2025-01-24 19:06:48 +0530

    fix: add bg color in group and add background hover in faq

--- src/components/cardComponent/LeftSideCard.vue
+++ src/components/cardComponent/LeftSideCard.vue
@@ -18,9 +18,9 @@ const props = defineProps({
 
 <template>
   <div
-    class="group flex flex-col h-full rounded-lg bg-cover bg-center p-2 lg:p-4 space-y-2 md:space-y-4  bg-gradient-gray grayscale group-hover:grayscale-0 group-hover:filter transition-all duration-300 border  border-transparent hover:border-gradient-blue"
+    class="group border flex flex-col h-full rounded-lg bg-cover bg-center p-2 lg:p-4 space-y-2 md:space-y-4 transition-all duration-300"
   >
-    <div>
+    <div class="">
       <img
         :src="card.image"
         :alt="card.title"
