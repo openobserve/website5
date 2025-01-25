cfdcb211eb5638bc7e320339111f82165452834e
ktx-monil <monil.desai@kiara.tech>
2025-01-25 13:49:35 +0530

    feat(component): add bg-img in featurecardwrapper

--- src/components/wrapper/FeatureCardWrapper.vue
+++ src/components/wrapper/FeatureCardWrapper.vue
@@ -2,6 +2,7 @@
 import CustomSection from "../core/CustomSection.vue";
 import CustomFeatureCard from "../cards/CustomFeatureCard.vue";
 import Heading from "../core/Heading.vue";
+
 const props = defineProps({
   items: {
     type: Object,
@@ -9,16 +10,27 @@ const props = defineProps({
   },
 });
 </script>
+
 <template>
-  <div
-    class="py-10 bg-cover bg-no-repeat overflow-visible"
-    style="
-      background-image: url('/Platform/FeatureCard-Ellipse 140.svg'),
-        url('/Platform/FeatureCard-Ellipse 136.svg');
-      background-repeat: no-repeat, no-repeat;
-      background-position: bottom center, left center; /* Set positions to avoid cutting */
-    "
-  >
+  <div class="relative py-10 bg-cover bg-no-repeat overflow-visible">
+    <!-- Background Images -->
+    <img
+      src="/Platform/FeatureCard-Ellipse 136.svg"
+      alt="Image"
+      class="absolute bottom-[-30vh] right-[110vh] opcacity-30 object-contain"
+    />
+    <img
+      src="/Platform/FeatureCard-Ellipse 140.svg"
+      alt="Image"
+      class="absolute bottom-[-60vh] right-[80vh] object-contain opacity-70"
+    />
+    <img
+      src="/Platform/Feature Card Ellipse 40.svg"
+      alt="Image"
+      class="absolute top-0 right-0 object-contain"
+    />
+
+    <!-- Main Content -->
     <CustomSection>
       <Heading :title="props.items.title" align="CENTER" />
       <CustomFeatureCard
@@ -30,4 +42,10 @@ const props = defineProps({
     </CustomSection>
   </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+/* Customize the image sizes and positions if needed */
+img {
+  z-index: -1; /* Ensure the images appear behind the content */
+}
+</style>
