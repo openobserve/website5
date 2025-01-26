503e9a85c0ade83e4dd57bfda29b78ad3e3cdcbb
krupa <krupa.jivani@kiara.tech>
2025-01-26 17:06:55 +0530

    fix: update component to vue3

--- src/components/core/CustomButton.vue
+++ src/components/core/CustomButton.vue
@@ -17,7 +17,7 @@
     <span class="relative z-10 flex items-center">
       <template v-if="$slots.default">
         <slot></slot>
-        <span class="ml-2">{{ buttonText }}</span>
+        <span v-if="buttonText" class="ml-2">{{ buttonText }}</span>
       </template>
       <template v-else>
         <span>{{ buttonText }}</span>
@@ -37,10 +37,7 @@ export default defineComponent({
       type: String,
       default: "primary",
       validator: (value) =>
-        ["primary", "secondary", "tertiary", "pricing", "ghost"].includes(
-        ["primary", "secondary", "tertiary", "pricing", "ghost"].includes(
-          value
-        ),
+        ["primary", "secondary", "tertiary", "pricing", "ghost"].includes(value),
     },
     size: {
       type: String,
@@ -83,27 +80,27 @@ export default defineComponent({
     const variants = {
       primary: {
         border:
-          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
+          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark via-button-primary-bg-blue-light to-button-primary-bg-sky-blue opacity-0 transition-opacity group-hover:opacity-100",
         background:
-          "absolute inset-[0.5px] rounded-full bg-background hover:bg-gradient-to-r hover:from-button-primary-bg-blue-dark hover:from-0% hover:via-button-primary-bg-blue-light hover:via-50% hover:to-button-primary-bg-sky-blue hover:to-100%",
+          "absolute inset-[0.5px] rounded-full bg-background hover:bg-gradient-to-r hover:from-button-primary-bg-blue-dark hover:via-button-primary-bg-blue-light hover:to-button-primary-bg-sky-blue",
       },
       secondary: {
         border:
-          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
+          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark via-button-primary-bg-blue-light to-button-primary-bg-sky-blue opacity-0 transition-opacity group-hover:opacity-100",
         background:
-          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-l hover:from-button-secondary-bg-dark-sky-blue hover:from-0% hover:via-button-secondary-bg-light-blue hover:via-50% hover:to-button-secondary-bg-light-sky-blue hover:to-100%",
+          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-l hover:from-button-secondary-bg-dark-sky-blue hover:via-button-secondary-bg-light-blue hover:to-button-secondary-bg-light-sky-blue",
       },
-      tartiary: {
+      tertiary: {
         border:
-          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
+          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark via-button-primary-bg-blue-light to-button-primary-bg-sky-blue opacity-0 transition-opacity group-hover:opacity-100",
         background:
-          "absolute inset-[0.7x] rounded-full hover:bg-gradient-to-t hover:from-button-secondary-bg-dark-sky-blue hover:from-0% hover:via-button-secondary-bg-light-blue hover:via-50% hover:to-button-secondary-bg-light-sky-blue hover:to-100%",
+          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-t hover:from-button-secondary-bg-dark-sky-blue hover:via-button-secondary-bg-light-blue hover:to-button-secondary-bg-light-sky-blue",
       },
       ghost: {
         border:
-          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
+          "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark via-button-primary-bg-blue-light to-button-primary-bg-sky-blue opacity-0 transition-opacity group-hover:opacity-100",
         background:
-          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-t hover:from-button-bg-gray-dark hover:from-0%  hover:to-button-bg-gray-light hover:to-100%",
+          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-t hover:from-button-bg-gray-dark hover:to-button-bg-gray-light",
       },
       pricing: {
         border: "",
@@ -118,34 +115,25 @@ export default defineComponent({
       large: "px-6 py-2.5 text-lg",
     };
 
-    const buttonClasses = computed(() => {
-      return [
-        "flex justify-center items-center w-full h-full text-center transition duration-300",
-        props.btnClass,
-      ]
-        .filter(Boolean)
-        .join(" ");
-    });
+    const buttonClasses = computed(() => [
+      "flex justify-center items-center w-full h-full text-center transition duration-300",
+      props.btnClass,
+    ]);
 
-    const backgroundClasses = computed(() => {
-      return ["", variants[props?.variant]?.background]
-        .filter(Boolean)
-        .join(" ");
-    });
+    const backgroundClasses = computed(() => variants[props.variant]?.background || "");
 
-    const borderClasses = computed(() => {
-      return ["", variants[props?.variant]?.border].filter(Boolean).join(" ");
-    });
+    const borderClasses = computed(() => variants[props.variant]?.border || "");
 
     const parentBackground = computed(() => {
-      if (props?.variant === "primary") {
-        return "bg-background text-white";
-      } else if (props?.variant === "tartiary") {
-        return "text-white";
-      } else if (props?.variant === "pricing") {
-        return "bg-white text-blue-500";
-      } else {
-        return "border border-white text-white hover:border-button-secondary-bg-dark-sky-blue";
+      switch (props.variant) {
+        case "primary":
+          return "bg-background text-white";
+        case "tertiary":
+          return "text-white";
+        case "pricing":
+          return "bg-white text-blue-500";
+        default:
+          return "border border-white text-white hover:border-button-secondary-bg-dark-sky-blue";
       }
     });
 
@@ -154,7 +142,7 @@ export default defineComponent({
       backgroundClasses,
       borderClasses,
       parentBackground,
-      sizes
+      sizes,
     };
   },
 });
