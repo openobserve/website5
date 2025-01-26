990d50147f979426d919ba4e6a2d14369da0ae62
krupa <krupa.jivani@kiara.tech>
2025-01-26 10:22:46 +0530

    fix: button padding

--- src/components/core/CustomButton.vue
+++ src/components/core/CustomButton.vue
@@ -2,7 +2,7 @@
   <component
     :is="buttonLink ? 'a' : 'button'"
     :class="[
-      'relative px-6 py-2 font-semibold overflow-hidden group rounded-full inline-flex',
+      'relative font-semibold overflow-hidden group rounded-full inline-flex',
       parentBackground,
       variant === 'pricing' ? '' : 'btnShadow',
     ]"
@@ -14,7 +14,7 @@
   >
     <span :class="borderClasses"></span>
     <span :class="backgroundClasses"></span>
-    <span class="relative z-10 flex items-center">
+    <span :class='["relative z-10 flex items-center", sizes[size]]'>
       <template v-if="$slots.default">
         <slot></slot>
         <span class="ml-2">{{ buttonText }}</span>
@@ -84,25 +84,25 @@ export default defineComponent({
         border:
           "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
         background:
-          "absolute inset-1 rounded-full bg-background hover:bg-gradient-to-r hover:from-button-primary-bg-blue-dark hover:from-0% hover:via-button-primary-bg-blue-light hover:via-50% hover:to-button-primary-bg-sky-blue hover:to-100%",
+          "absolute inset-[0.5px] rounded-full bg-background hover:bg-gradient-to-r hover:from-button-primary-bg-blue-dark hover:from-0% hover:via-button-primary-bg-blue-light hover:via-50% hover:to-button-primary-bg-sky-blue hover:to-100%",
       },
       secondary: {
         border:
           "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
         background:
-          "absolute inset-1 rounded-full hover:bg-gradient-to-l hover:from-button-secondary-bg-dark-sky-blue hover:from-0% hover:via-button-secondary-bg-light-blue hover:via-50% hover:to-button-secondary-bg-light-sky-blue hover:to-100%",
+          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-l hover:from-button-secondary-bg-dark-sky-blue hover:from-0% hover:via-button-secondary-bg-light-blue hover:via-50% hover:to-button-secondary-bg-light-sky-blue hover:to-100%",
       },
       tartiary: {
         border:
           "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
         background:
-          "absolute inset-1 rounded-full hover:bg-gradient-to-t hover:from-button-secondary-bg-dark-sky-blue hover:from-0% hover:via-button-secondary-bg-light-blue hover:via-50% hover:to-button-secondary-bg-light-sky-blue hover:to-100%",
+          "absolute inset-[0.7x] rounded-full hover:bg-gradient-to-t hover:from-button-secondary-bg-dark-sky-blue hover:from-0% hover:via-button-secondary-bg-light-blue hover:via-50% hover:to-button-secondary-bg-light-sky-blue hover:to-100%",
       },
       ghost: {
         border:
           "absolute inset-0 bg-gradient-to-t from-button-primary-bg-blue-dark from-0% via-button-primary-bg-blue-light via-50% to-button-primary-bg-sky-blue to-100% transition-opacity opacity-0 group-hover:opacity-100",
         background:
-          "absolute inset-1 rounded-full hover:bg-gradient-to-t hover:from-button-bg-gray-dark hover:from-0%  hover:to-button-bg-gray-light hover:to-100%",
+          "absolute inset-[0.7px] rounded-full hover:bg-gradient-to-t hover:from-button-bg-gray-dark hover:from-0%  hover:to-button-bg-gray-light hover:to-100%",
       },
       pricing: {
         border: "",
@@ -112,15 +112,14 @@ export default defineComponent({
 
     // Tailwind classes for button sizes
     const sizes = {
-      small: "px-2 py-2 text-sm",
-      medium: "px-4 py-3 text-md",
-      large: "px-6 py-4 text-lg",
+      small: "px-2 py-0.5 text-xs",
+      medium: "px-4 py-1 text-md",
+      large: "px-6 py-2.5 text-lg",
     };
 
     const buttonClasses = computed(() => {
       return [
         "flex justify-center items-center w-full h-full text-center transition duration-300",
-        sizes[props.size],
         props.btnClass,
       ]
         .filter(Boolean)
@@ -145,7 +144,7 @@ export default defineComponent({
       } else if (props?.variant === "pricing") {
         return "bg-white text-blue-500";
       } else {
-        return "border border-white text-white hover:border-button-primary-bg-blue-dark";
+        return "border border-white text-white hover:border-button-secondary-bg-dark-sky-blue";
       }
     });
 
@@ -154,6 +153,7 @@ export default defineComponent({
       backgroundClasses,
       borderClasses,
       parentBackground,
+      sizes
     };
   },
 });
