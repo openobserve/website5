ee8c96becee87d0d881beb9ff3806c5a7a3aa5b5
ktx-monil <monil.desai@kiara.tech>
2025-01-30 14:45:09 +0530

    fix: make required css changes in resource hero section

--- src/components/heroSections/ResourceHeroSection.vue
+++ src/components/heroSections/ResourceHeroSection.vue
@@ -16,7 +16,7 @@ const props = defineProps({
 
 <template>
   <section
-    class="relative flex flex-col items-center justify-end text-center px-4 w-full min-h-[60vh] before:content-[''] before:absolute before:w-full before:min-h-[200vh] before:top-0 before:bg-no-repeat before:bg-[50%_-40vh] md:before:bg-[50%_-50vh] lg:before:bg-[50%_-20vh] xl:before:bg-[50%_-10vh] before:bg-[url('/resources/BG.svg')]"
+    class="relative flex flex-col items-center justify-end text-center px-4 w-full min-h-[40vh] before:content-[''] before:absolute before:w-full before:min-h-[160vh] before:top-0 before:bg-no-repeat before:bg-[50%_-50vh] xl:before:bg-[50%_-30vh] before:bg-[url('/resources/BG.svg')]"
   >
     <div class="z-10 container mx-auto max-w-4xl">
       <TextWithGradient
