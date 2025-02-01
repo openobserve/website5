5d5a47113537c56835d66aedbebbfa6ac863009c
Kirtan Thakkar <kirtan@kiara.tech>
2025-02-01 16:46:16 +0530

    fix: image icons

--- src/components/cardComponent/UsecaseCardWithIcon.vue
+++ src/components/cardComponent/UsecaseCardWithIcon.vue
@@ -26,7 +26,7 @@ const dynamicComponent = props.card.link ? "a" : "div";
   ]">
     <!-- Content Layer -->
     <div class="flex flex-col space-y-4 w-full h-full items-center justify-center rounded-2xl bg-[#23282c] p-8">
-      <CustomImage :image="card.icon" altText="Icon" cssClass="w-16 h-16 object-cover" />
+      <CustomImage :image="card.image" altText="Icon" cssClass="h-16" />
       <h3 class="text-[#BEC0C2] text-base font-semibold text-center" :title="card.title">
         {{ card.title }}
       </h3>
