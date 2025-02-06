41c71d52e51e096486c3555b9f96bc0fac693441
ktx-riya <riya.kukadiya@kiara.tech>
2025-02-06 11:56:33 +0530

    fix gradient border issue in customcardwrapper component

--- src/components/cards/CustomCard.vue
+++ src/components/cards/CustomCard.vue
@@ -17,9 +17,9 @@ function truncate(text, maxLength = 200) {
 </script>
 
 <template>
-
+<div class="relative rounded-lg w-full p-[0.0625rem] bg-gradient-blue transition-all duration-300 hover:shadow-[0_0_60px_0_rgba(66,174,255,0.8)] container mx-auto">
   <!-- Content Layer -->
-  <div class="flex flex-col space-y-4 w-full h-full items-center rounded-lg bg-[#23282c] p-8 cardShadow">
+  <div class="flex flex-col space-y-4 w-full h-full items-center rounded-lg bg-[#23282c] p-8">
     <CustomImage :image="cards.image" altText="Icon" cssClass="h-16" />
     <h3 class="text-[#BEC0C2] text-2xl font-bold truncate" :title="cards.title">
       {{ cards.title }}
@@ -28,12 +28,5 @@ function truncate(text, maxLength = 200) {
       {{ truncate(cards.description, 200) }}
     </p>
   </div>
+</div>
 </template>
-
-<style scoped>
-.cardShadow:hover {
-  box-shadow: 0 0 60px 0 rgba(66, 174, 255, 0.8);
-  /* Shadow effect */
-  border-radius: 0.5rem;
-}
-</style>
