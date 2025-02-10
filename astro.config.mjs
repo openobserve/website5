3033dc667ffd5e8cc8e0b7b78db849b2a142e2bc
Kirtan Thakkar <kirtan@kiara.tech>
2025-02-10 20:14:46 +0530

    fix: remove redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-
-import vue from '@astrojs/vue';
-import tailwind from '@astrojs/tailwind';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-website.com', // Replace with your website URL
+  site: "https://your-website.com", // Replace with your website URL
   integrations: [vue(), tailwind(), sitemap()],
-  redirects: {
-    "/resources/[...slug]": "/articles/[...slug]",
-    "/resources/page/[...slug]": "/articles/page/[...slug]",
-    "/resources/tag/[...slug]": "/articles/tag/[...slug]",
-    "/resources/author/[...slug]": "/articles/author/[...slug]",
+  // redirects: {
+  //   "/resources/[...slug]": "/articles/[...slug]",
+  //   "/resources/page/[...slug]": "/articles/page/[...slug]",
+  //   "/resources/tag/[...slug]": "/articles/tag/[...slug]",
+  //   "/resources/author/[...slug]": "/articles/author/[...slug]",
+  // },
+  build: {
+    concurrency: 2,
   },
 });
