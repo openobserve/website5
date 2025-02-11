1bc4f6d60b4559fd41e7abf6f172ab4abacf88ee
Kirtan Thakkar <kirtan@kiara.tech>
2025-02-11 19:54:13 +0530

    fix: resoruces

--- src/components/blogs/SingleAuthorDetails.vue
+++ src/components/blogs/SingleAuthorDetails.vue
@@ -62,9 +62,9 @@ const props = defineProps({
           class="w-full md:w-1/6 h-44 md:h-auto relative flex items-center justify-center bg-gray-800"
         >
           <img
-            v-if="author.image.url"
-            :src="author.image.url"
-            :alt="author.name"
+            v-if="author?.image?.url"
+            :src="author?.image?.url"
+            :alt="author?.name"
             class="w-full h-full object-contain absolute inset-0"
           />
           <div v-else class="w-full h-full flex items-center justify-center text-white">
