b9852792222a6f51f5610da875dca78965d135f6
ktx-monil <monil.desai@kiara.tech>
2025-02-11 10:08:24 +0530

    fix(component): change structure format of component code

--- src/components/cardComponent/FeatureCardWithIconWrapper.vue
+++ src/components/cardComponent/FeatureCardWithIconWrapper.vue
@@ -18,16 +18,32 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="relative container mx-auto bg-cover bg-no-repeat w-full overflow-visible">
-    <img src="/cardBg1.svg" class="absolute top-[0%] left-[-20%] -z-10 object-cover" alt="Background SVG" />
-    <img src="/cardBg2.svg" alt="Image" class="absolute bottom-[-60vh] -z-10 object-cover" />
+  <div
+    class="relative container mx-auto bg-cover bg-no-repeat w-full overflow-visible"
+  >
+    <img
+      src="/cardBg1.svg"
+      class="absolute top-[0%] left-[-20%] -z-10 object-cover"
+      alt="Background SVG"
+    />
+    <img
+      src="/cardBg2.svg"
+      alt="Image"
+      class="absolute bottom-[-60vh] -z-10 object-cover"
+    />
 
     <CustomSection>
       <Heading :title="heading?.title" :description="heading?.subtitle" />
 
-      <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-2 md:gap-6 mt-10 w-full">
+      <div
+        class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-2 md:gap-6 mt-10 w-full"
+      >
         <!-- Loop moved here -->
-        <IconTitleCard v-for="(item, index) in data" :key="index" :card="item" />
+        <IconTitleCard
+          v-for="(item, index) in data"
+          :key="index"
+          :card="item"
+        />
       </div>
     </CustomSection>
   </div>
