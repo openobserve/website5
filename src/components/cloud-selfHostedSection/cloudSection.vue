82ae8295844d9bf704f90818379e64a49e2785ec
Nitya Timalsina <14898800+nityatimalsina@users.noreply.github.com>
2025-02-14 14:58:15 -0700

    Temporary fix of login option on Downloads page

--- src/components/cloud-selfHostedSection/cloudSection.vue
+++ src/components/cloud-selfHostedSection/cloudSection.vue
@@ -97,6 +97,17 @@ const handleSubmit = (values) => {
         </h2>
         <div class="text-sm md:text-md font-medium text-white text-left mb-8">By using this service, you agree to OpenObserve's Terms and Conditions.</div>
         <div class="space-y-4 mb-8 flex flex-col items-center justify-center">
+          <a href="https://cloud.openobserve.ai/web/login" class="w-full md:w-80">
+            <button
+              class="flex justify-start gap-5 md:gap-10 w-full bg-gray-800 text-gray-200 rounded-lg p-2 md:p-3 shadow-md hover:bg-gray-700 transition"
+            >
+              <img src="/logo.svg" alt="OpenObserve" class="h-6 w-6 mr-3" />
+              <span class="font-medium text-sm md:text-base">Try OpenObserve Cloud</span>
+            </button>
+          </a>
+        </div>
+
+        <!-- <div class="space-y-4 mb-8 flex flex-col items-center justify-center">
           <a href="https://auth.openobserve.ai/dex/auth/github?client_id=o2-client&redirect_uri=https%3A%2F%2Fcloud.openobserve.ai%2Fconfig%2Fredirect&response_type=code&scope=openid+profile+email+groups+offline_access&state=5cuFXm2a0iMLlHZvJNCJVK9NgTPUNWLp" class="w-full md:w-80">
             <button
               class="flex justify-start gap-5 md:gap-10 w-full bg-gray-800 text-gray-200 rounded-lg p-2 md:p-3 shadow-md hover:bg-gray-700 transition"
@@ -136,7 +147,7 @@ const handleSubmit = (values) => {
               <span class="font-medium text-sm md:text-base">Log in with Microsoft</span>
             </button>
           </a>
-        </div>
+        </div> -->
 
         <!-- <CustomSeprater /> -->
 
