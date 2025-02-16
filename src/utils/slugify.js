64819685655c73f35e11d2246598fc7ef4aa6279
ktx-dhrumil <dhrumil.trada@kiara.tech>
2025-02-16 10:59:45 +0530

    bugfix: changes in redirection and author and blogs (#98)

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,18 +1,15 @@
 export function slugify(title) {
   return title
-    .toLowerCase() // Convert to lowercase
-    .trim() // Trim whitespace from both ends
-    .replace(/\s+/g, "-") // Replace spaces with hyphens
-    .replace(/[^a-z0-9-]/g, ""); // Remove non-alphanumeric characters except hyphens
+  .toString()
+  .normalize("NFD")
+  .replace(/[\u0300-\u036f]/g, "") // Remove accents
+  .toLowerCase()
+  .trim()
+  .replace(/[^a-z0-9 ]/g, "") // Remove special characters
+  .replace(/\s+/g, "-")// Remove non-alphanumeric characters except hyphens
 }
 
-// const slugify = (str) => {
-//   return str
-//     .toString()
-//     .normalize("NFD")
-//     .replace(/[\u0300-\u036f]/g, "") // Remove accents
-//     .toLowerCase()
-//     .trim()
-//     .replace(/[^a-z0-9 ]/g, "") // Remove special characters
-//     .replace(/\s+/g, "-"); // Replace spaces with "-"
-// };
+// title.toLowerCase() // Convert to lowercase
+//     .trim() // Trim whitespace from both ends
+//     .replace(/\s+/g, "-") // Replace spaces with hyphens
+//     .replace(/[^a-z0-9-]/g, "");
