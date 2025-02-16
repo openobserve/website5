beee977a5f069e362439c0c2350e57eed2a2d247
ktx-dhrumil <dhrumil.trada@kiara.tech>
2025-02-16 09:53:05 +0530

    bugfix: changes in redirection and author and blogs

--- src/utils/redirection.js
+++ src/utils/redirection.js
@@ -1,13 +1,13 @@
 import { slugify } from "./slugify";
 
 export const generateNavLink = (type, name) => {
-    return `/${type}/${slugify(name)}`;
+    return `/${type}/${name}`;
   };
   export const generateTagLink = (type, name) => {
     return `/${type}/tag/${slugify(name)}`;
   };
   export const generateAuthorLink = (type, name) => {
-    return `/${type}/author/${slugify(name)}`;
+    return `/${type}/author/${name}`;
   };
   export const generatePageNavLink = (type, currentPage) => {
     const basePath = type === "blog" ? "blog" : "articles";
