bb68eb08b1e3a54ba8ec48978f6203f3cae57022
ktx@riddhi <riddhi.halani@kiara.tech>
2025-02-18 16:15:24 +0530

    bugfix(navigation): category and navigation issue resolved

--- src/components/wrapper/CustomSuggestionsWrapper.vue
+++ src/components/wrapper/CustomSuggestionsWrapper.vue
@@ -9,16 +9,15 @@ const props = defineProps({
 </script>
 
 <template>
-   <!-- <h1 class="text-white font-semibold py-6">Tags</h1> -->
-  <div class="flex flex-wrap gap-2 w-full  justify-center max-h-full overflow-visible">
-
+  <div class="flex flex-wrap justify-center items-center content-center gap-2 w-full max-h-full overflow-visible">
     <a
       v-for="(suggestion, index) in suggestions"
       :key="index"
       :href="generateTagLink(type, suggestion.name)"
-      class="px-4 md:px-6 py-1 bg-black border rounded-full hover:text-white text-xs md:text-sm text-neutral-400"
+      class="px-4 md:px-6 py-1 bg-black border rounded-full text-center hover:text-white text-xs md:text-sm text-neutral-400"
     >
       {{ suggestion.name }}
-  </a>
+    </a>
   </div>
 </template>
+
