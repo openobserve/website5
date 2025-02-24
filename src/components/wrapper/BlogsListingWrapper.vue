332b585f6fde45315d74017ce4dadd15c2ff7b03
ktx-riya <riya.kukadiya@kiara.tech>
2025-02-24 15:33:03 +0530

    fix(props):add the searchbar props in the blogslistingwrapper component

--- src/components/wrapper/BlogsListingWrapper.vue
+++ src/components/wrapper/BlogsListingWrapper.vue
@@ -1,16 +1,7 @@
 <template>
   <section class="container mx-auto px-4 md:px-6 lg:px-8 xl:px-11 w-full flex flex-col justify-start">
-    <div class="py-3 md:py-6">
-      <CustomSuggestions :suggestions="suggestionData" @selectSuggestion="selectSuggestion" :type="type" />
-      <div class="w-full md:w-3/5 justify-center md:justify-end mt-4 md:mt-0">
-        <!-- <div class="bg-black border border-gray-500 rounded-md p-2">
-            <button class="flex items-center justify-center">
-              <img src="/filter.svg" alt="Filter Icon" class="w-6 h-6" />
-            </button>
-          </div> -->
-      </div>
-    </div>
-    <div class="search-container relative w-full px-4 pb-5">
+
+    <div v-if="searchBar" class="search-container relative w-full px-4 pb-5">
       <div
         class="flex flex-row w-full space-x-2 justify-between items-center text-white border border-gray-50/10 h-10 rounded-lg px-2 focus-within:border-sky-500 bg-gray-700">
         <span class="text-white px-4">
@@ -31,9 +22,8 @@
           </p>
         </template>
       </div>
-
-
     </div>
+
     <div class="block md:hidden">
       <BlogsListingMobileWrapper :sectionData="filteredBlogsData" titleTextColor="text-white"
         descriptionTextColor="text-gray-400" cardBgColor="bg-[#2A2A2A]" linkColor="text-[#00A3FF]" :type="type" />
@@ -58,16 +48,15 @@ import CustomSuggestions from "./CustomSuggestionsWrapper.vue";
 import BlogListing from "../blogs/BlogListing.vue";
 import BlogPagination from "../blogs/BlogPagination.vue";
 import { itemsPerPage, getAllBlogs } from "@/utils/api/blogs";
+import { boolean } from "astro:schema";
 
 const props = defineProps({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
   blogsData: { type: Array, required: true },
   allBlogData: { type: Array, required: true },
   currentPage: { type: Number, required: true },
-  suggestionData: { type: Array, required: true },
   totalItems: { type: Number, required: true },
   type: { type: String, required: true },
+  searchBar: {type: Boolean, required: false},
 });
 
 const searchQuery = ref(""); // category click event
