57520d78d0b5e37239e4e22de6fa7cd0ec654aff
Monil-KTX <monil.desai@kiara.tech>
2025-03-10 12:16:50 +0530

    bugfix/change-text-sizing-issue-and-title-change-in-header-link (#154)
    * fix(LaptopView,MobileView): Change text size of resouce link
    
    * feat(page): add client:load for click event in thank-you page
    
    * fix(sampleForm): change passing value of data volume when user submit form
    
    * feat(CustomFeaturCardResource, CardWrapper): add link in card
    
    * feat(CustomFeatureCardResource): add hover effect in card

--- src/components/cards/CustomFeatureCardResource.vue
+++ src/components/cards/CustomFeatureCardResource.vue
@@ -25,32 +25,26 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="container mx-auto">
-    <!-- Layout changes based on screen size -->
-    <div class="flex flex-col md:flex-row items-center md:items-start gap-6">
-      <!-- Icon -->
-      <div class="h-14 w-14 flex-shrink-0">
-        <CustomRoundShape
-          :imgSrc="card.image"
-          iconSize="small"
-          :borderColor="borderColor"
-          size="small"
-        />
-      </div>
+  <a :href="card.link" target="_blank">
+    <div
+      class="container mx-auto border border-transparent hover:border hover:border-[#838484] transition-all duration-150 p-3 rounded-lg">
+      <!-- Layout changes based on screen size -->
+      <div class=" flex flex-col md:flex-row items-center md:items-start gap-6 ">
+        <!-- Icon -->
+        <div class=" h-14 w-14 flex-shrink-0">
+          <CustomRoundShape :imgSrc="card.image" iconSize="small" :borderColor="borderColor" size="small" />
+        </div>
 
-      <!-- Title & Description -->
-      <div class="flex flex-col text-center md:text-left">
-        <h2
-          :class="`font-semibold text-[16px] sm:text-[18px] md:text-[20px] m-0 ${titleTextColor}`"
-        >
-          {{ card.title }}
-        </h2>
-        <p
-          :class="`text-[12px] sm:text-[14px] md:text-[16px] m-0 ${descriptionTextColor}`"
-        >
-          {{ card.description }}
-        </p>
+        <!-- Title & Description -->
+        <div class="flex flex-col text-center md:text-left">
+          <h2 :class="`font-semibold text-[16px] sm:text-[18px] md:text-[20px] m-0 ${titleTextColor}`">
+            {{ card.title }}
+          </h2>
+          <p :class="`text-[12px] sm:text-[14px] md:text-[16px] m-0 ${descriptionTextColor}`">
+            {{ card.description }}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
+  </a>
 </template>
