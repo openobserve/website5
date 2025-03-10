f7fbd8a992c4f9f73ee112d62e9d0bb3cc8945c3
ktx-monil <monil.desai@kiara.tech>
2025-03-10 13:37:00 +0530

    fix: solve merge conflicts

--- src/components/cards/CustomSupportCard.vue
+++ src/components/cards/CustomSupportCard.vue
@@ -79,27 +79,24 @@ const props = defineProps({
           </div>
         </div>
 
-
-
         <div class="flex flex-row justify-center mt-5 space-x-2">
-          <div v-if="card?.primaryButton">
-            <CustomButton variant="secondary" :buttonText="card?.primaryButton?.text"
-              :buttonLink="card?.primaryButton?.link" :target="card?.primaryButton?.target">
-              <CustomImage cssClass="w-10 h-5" v-if="card.primaryButton?.icon" :image="card?.primaryButton?.icon" />
+          <div v-if="item?.primaryButton">
+            <CustomButton variant="secondary" :buttonText="item?.primaryButton?.text"
+              :buttonLink="item?.primaryButton?.link" :target="item?.primaryButton?.target">
+              <CustomImage cssClass="w-10 h-5" v-if="item.primaryButton?.icon" :image="item?.primaryButton?.icon" />
             </CustomButton>
           </div>
-          <div v-if="card?.secondaryButton">
-            <CustomButton variant="secondary" :buttonText="card?.secondaryButton?.text"
-              :buttonLink="card?.secondaryButton?.link" :target="card?.secondaryButton?.target">
-              <CustomImage cssClass="w-10 h-5" v-if="card?.secondaryButton?.icon"
-                :image="card?.secondaryButton?.icon" />
+          <div v-if="item?.secondaryButton">
+            <CustomButton variant="secondary" :buttonText="item?.secondaryButton?.text"
+              :buttonLink="item?.secondaryButton?.link" :target="item?.secondaryButton?.target">
+              <CustomImage cssClass="w-10 h-5" v-if="item?.secondaryButton?.icon"
+                :image="item?.secondaryButton?.icon" />
             </CustomButton>
           </div>
         </div>
       </div>
     </div>
   </CustomSection>
-
 </template>
 <style scoped>
 .car-bg {
