f5461e3ba92e2e7861ae56ef8a69809de2894409
ktx-monil <monil.desai@kiara.tech>
2025-03-10 11:08:33 +0530

    feat(CustomFeaturCardResource, CardWrapper): add link in card

--- src/components/cards/CustomFeatureCardResource.vue
+++ src/components/cards/CustomFeatureCardResource.vue
@@ -27,30 +27,23 @@ const props = defineProps({
 <template>
   <div class="container mx-auto">
     <!-- Layout changes based on screen size -->
-    <div class="flex flex-col md:flex-row items-center md:items-start gap-6">
-      <!-- Icon -->
-      <div class="h-14 w-14 flex-shrink-0">
-        <CustomRoundShape
-          :imgSrc="card.image"
-          iconSize="small"
-          :borderColor="borderColor"
-          size="small"
-        />
-      </div>
+    <a :href="card.link" target="_blank">
+      <div class="flex flex-col md:flex-row items-center md:items-start gap-6">
+        <!-- Icon -->
+        <div class="h-14 w-14 flex-shrink-0">
+          <CustomRoundShape :imgSrc="card.image" iconSize="small" :borderColor="borderColor" size="small" />
+        </div>
 
-      <!-- Title & Description -->
-      <div class="flex flex-col text-center md:text-left">
-        <h2
-          :class="`font-semibold text-[16px] sm:text-[18px] md:text-[20px] m-0 ${titleTextColor}`"
-        >
-          {{ card.title }}
-        </h2>
-        <p
-          :class="`text-[12px] sm:text-[14px] md:text-[16px] m-0 ${descriptionTextColor}`"
-        >
-          {{ card.description }}
-        </p>
+        <!-- Title & Description -->
+        <div class="flex flex-col text-center md:text-left">
+          <h2 :class="`font-semibold text-[16px] sm:text-[18px] md:text-[20px] m-0 ${titleTextColor}`">
+            {{ card.title }}
+          </h2>
+          <p :class="`text-[12px] sm:text-[14px] md:text-[16px] m-0 ${descriptionTextColor}`">
+            {{ card.description }}
+          </p>
+        </div>
       </div>
-    </div>
+    </a>
   </div>
 </template>
