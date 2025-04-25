a93d362e05b482e5c4fd21fdd773b8f6cf54a40f
ktx-monil <monil.desai@kiara.tech>
2025-04-25 16:31:57 +0530

    feat: add required component for header

---
+++ src/components/header/CustomHeaderButton.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({ title: String, linkTitle: String, link: String });
+</script>
+<template>
+  <div class="bg-black bg-opacity-40 card-border p-4 w-full">
+    <div class="flex flex-col lg:flex-row justify-between items-center space-y-4 lg:space-y-0">
+      <h3 class="text-white text-base">{{ title }}</h3>
+      <a class="gradient-hover cursor-pointer text-base" :href="link">
+        {{ linkTitle }}
+      </a>
+    </div>
+  </div>
+</template>
+<style scoped>
+/* .gradient-hover {
+  background: linear-gradient(
+    to left,
+    rgb(var(--blue-light)),
+    rgb(var(--blue-dark))
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+} */
+.gradient-hover {
+  background: linear-gradient(to left, #09e6ff, #00ffc3);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+
+.card-border {
+  border: 1px solid #313539;
+  border-radius: 0.5rem;
+}
+</style>
---
+++ src/components/header/CustomHoverHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-[url('/img/bg/gradient-bg/MenuTexture.svg')] w-full rounded-lg">
+    <div class="w-full bg-gradient-to-b from-gray-900 to-gray-800 text-white p-6 rounded-lg">
+      <!-- Default Slot where the parent passes all content -->
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomHoverHeader",
+};
+</script>
