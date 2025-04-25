31d4eae350b85e9aae3e02bc6d98912db813a63d
ktx-monil <monil.desai@kiara.tech>
2025-04-25 16:36:59 +0530

    fix: add required slugify file in utils

---
+++ src/utils/slugify.js
@@ -0,0 +1,15 @@
+export function slugify(title) {
+  return title
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // Remove accents
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 ]/g, "") // Remove special characters
+    .replace(/\s+/g, "-"); // Remove non-alphanumeric characters except hyphens
+}
+
+// title.toLowerCase() // Convert to lowercase
+//     .trim() // Trim whitespace from both ends
+//     .replace(/\s+/g, "-") // Replace spaces with hyphens
+//     .replace(/[^a-z0-9-]/g, "");
