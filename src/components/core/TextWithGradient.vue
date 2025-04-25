7774ccb70907466e3324b99dea04c1d0a367987d
ktx-monil <monil.desai@kiara.tech>
2025-04-25 15:54:56 +0530

    fix: add new component for gradiant text

---
+++ src/components/core/TextWithGradient.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { computed } from "vue";
+
+// Define props using defineProps
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+  textGradientColor: {
+    type: String,
+    required: false,
+  },
+  textSize: {
+    type: String,
+    required: false,
+  },
+  padding: {
+    type: String,
+    required: false,
+  },
+  align: {
+    type: String,
+    default: "left",
+    validator: (value) => ["left", "center", "right"].includes(value),
+  },
+});
+
+// Computed property for the class
+const computedClass = computed(() => {
+  return `font-semibold text-[${props.textSize}px] text-${props.align} ${props.textGradientColor} text-gradient ${props.padding}`;
+});
+</script>
+
+<template>
+  <h1 :class="computedClass">
+    {{ title }}
+  </h1>
+</template>
+
+<style>
+.text-gradient {
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+</style>
+
+
+
+<!-- <TextGradient
+  title="Open Source Observability"
+  textSize="[48px]" 
+  textGradientColor="bg-gradient-blue"
+  align="center"
+/> -->
