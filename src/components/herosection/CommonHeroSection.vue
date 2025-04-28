949f53fc3ab71b57a3101b88b40c242311d5f230
ktx-riya <riya.kukadiya@kiara.tech>
2025-04-28 13:28:27 +0530

    build the common hero section component.

---
+++ src/components/herosection/CommonHeroSection.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  align: {
+    type: String as () => 'left' | 'center' | 'right',
+    default: 'left',
+  },
+})
+</script>
+
+<template>
+
+  <div class="w-full bg-gradient-to-r from-indigo-400 via-blue-400 to-sky-500 py-20 px-5">
+    <div
+      class="max-w-full mx-auto flex flex-col"
+      :class="{
+        'items-start text-left': align === 'left',
+        'items-center text-center': align === 'center',
+        'items-end text-right': align === 'right'
+      }"
+    >
+      <h1 class="text-4xl font-bold text-white mb-4">
+        {{ title }}
+      </h1>
+      <p class="text-white text-lg">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+
+</template>
