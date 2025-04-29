7dc8b642d4c6f3ee60a61d9b17170877ea758749
ktx-riya <riya.kukadiya@kiara.tech>
2025-04-29 13:29:46 +0530

    change effect of button.

--- src/components/core/CustomButton.vue
+++ src/components/core/CustomButton.vue
@@ -155,8 +155,7 @@ const onClick = (event) => {
 }
 .secondary-button:hover{
   /* Transparent border to show gradient */
-  background-color:#5764d9;
-  color: white;
+  background-color:#f3f4f6;
 
 }
 .secondary-button:disabled{
@@ -194,6 +193,7 @@ const onClick = (event) => {
   box-shadow: none;
 }
 
+
 .tertiary-button {
   position: relative;
   color: white;
@@ -206,9 +206,7 @@ const onClick = (event) => {
   transition: all;
 }
 .tertiary-button:hover{
-  border: 1px solid black;
-  /* Transparent border to show gradient */
-  color: black;
+  background: rgba(255, 255, 255, 0.1); 
 
 }
 .tertiary-button:disabled{
@@ -261,7 +259,7 @@ const onClick = (event) => {
   transition: all 0.3s ease;
   background-color: black;
   /* Creates a 2px border that matches the button color initially */
-  border: 1px solid transparent;
+  border: 1px solid #6B76E3 ;
 }
 
 .black-primary-button:hover {
