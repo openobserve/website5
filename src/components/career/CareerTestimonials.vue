237ef212cee27b4a188ea229fd44f0b6ca832f4b
ktx-monil <monil.desai@kiara.tech>
2025-04-29 09:29:11 +0530

    fix: change css of careerTestimonial component

--- src/components/career/CareerTestimonials.vue
+++ src/components/career/CareerTestimonials.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="bg-light-gray">
-    <div class="container mx-auto py-16 px-4 ">
-      <div class="flex flex-col items-center space-y-4 text-center mb-10">
+  <section class="w-full py-12 md:py-14 bg-light-gray">
+    <div class="container mx-auto py-16 px-4 md:px-6">
+      <div class="flex flex-col items-start space-y-4 mb-10">
         <h2 class="text-3xl font-bold tracking-tighter sm:text-4xl">{{ title }}</h2>
-        <p class="mx-auto max-w-[700px] text-muted-foreground">
+        <p class="mx-auto max-w-[700px] text-muted-foreground text-left">
           {{ description }}
         </p>
       </div>
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <div v-for="(testimonial, index) in items" :key="index"
-          class="p-6 rounded-lg border bg-card text-card-foreground shadow-sm space-y-4">
-          <p class="italic text-muted-foreground ">"{{ testimonial.quote }}"</p>
-          <div class="flex items-center">
-            <div class="w-12 h-12 rounded-full overflow-hidden bg-gray-600 mr-4">
+          class="p-6 rounded-lg border bg-card text-card-foreground shadow-sm space-y-4 flex flex-col justify-between">
+          <p class="italic text-muted-foreground text-left">"{{ testimonial.quote }}"</p>
+          <div class="flex items-center space-x-4 mt-4">
+            <div class="w-12 h-12 rounded-full overflow-hidden bg-gray-600">
               <img v-if="testimonial.avatar" :src="testimonial.avatar" :alt="testimonial.name"
                 class="w-full h-full object-cover">
             </div>
             <div>
-              <p class="font-bold">{{ testimonial.name }}</p>
-              <p class="text-gray-300 text-sm">{{ testimonial.role }}</p>
+              <p class="font-bold text-left">{{ testimonial.name }}</p>
+              <p class="text-sm text-muted-foreground text-left">{{ testimonial.role }}</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
