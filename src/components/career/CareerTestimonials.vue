b1a685ac60e03fc1a68fc4d3ece2585e21254403
ktx-monil <monil.desai@kiara.tech>
2025-04-29 09:41:37 +0530

    feat: add image in testimonial

--- src/components/career/CareerTestimonials.vue
+++ src/components/career/CareerTestimonials.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="w-full py-12 md:py-14 bg-light-gray">
     <div class="container mx-auto py-16 px-4 md:px-6">
-      <div class="flex flex-col items-start space-y-4 mb-10">
+      <div class="flex flex-col items-center space-y-4 mb-10 text-center">
         <h2 class="text-3xl font-bold tracking-tighter sm:text-4xl">{{ title }}</h2>
-        <p class="mx-auto max-w-[700px] text-muted-foreground text-left">
+        <p class="mx-auto max-w-[700px] text-muted-foreground ">
           {{ description }}
         </p>
       </div>
@@ -13,9 +13,8 @@
           class="p-6 rounded-lg border bg-card text-card-foreground shadow-sm space-y-4 flex flex-col justify-between">
           <p class="italic text-muted-foreground text-left">"{{ testimonial.quote }}"</p>
           <div class="flex items-center space-x-4 mt-4">
-            <div class="w-12 h-12 rounded-full overflow-hidden bg-gray-600">
-              <img v-if="testimonial.avatar" :src="testimonial.avatar" :alt="testimonial.name"
-                class="w-full h-full object-cover">
+            <div class="w-12 h-12 rounded-full overflow-hidden">
+              <img :src="testimonial.avatar" :alt="testimonial.name" class="w-12 h-12 rounded-full object-cover">
             </div>
             <div>
               <p class="font-bold text-left">{{ testimonial.name }}</p>
