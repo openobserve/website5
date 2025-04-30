d7fcb06d97ca0e6c13f5fcd7041f8821d40ab4f6
ktx-monil <monil.desai@kiara.tech>
2025-04-30 11:02:17 +0530

    feat: add collectionMapper component change json content and make it like strapi call data and pass to components

---
+++ src/components/ComponentCollectionMapper.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <component v-for="(it, index) in data" :key="index" :is="componentsMap[it.__component]"
+      v-bind="getComponentProps(it)" />
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+// Import all possible section components
+import CareerHero from "@/components/career/CareerHero.vue";
+import CareerMission from "@/components/career/CareerMission.vue";
+import CareerBenefits from "@/components/career/CareerBenefits.vue";
+import CareerGlobalTeam from "@/components/career/CareerGlobalTeam.vue";
+import CareerTestimonials from "@/components/career/CareerTestimonials.vue";
+import CareerTechnology from "@/components/career/CareerTechnology.vue";
+import CareerCTA from "@/components/career/CareerCTA.vue";
+
+
+const props = defineProps({
+  data: { type: Array, required: true },
+  blogsData: { type: Array, required: false },
+  blogs: { type: Array, required: false },
+  caseStudyData: { type: Array, required: false },
+  bannerData: { type: Object, required: false },
+});
+
+const componentsMap = computed(() => ({
+  "section-hero.careers-hero": CareerHero,
+  "section-text.mission-statement": CareerMission,
+  "section-cards.career-benefits": CareerBenefits,
+  "section-features.global-team": CareerGlobalTeam,
+  "section-testimonials.employee-testimonials": CareerTestimonials,
+  "section-features.technology": CareerTechnology,
+  "section-features.cta": CareerCTA,
+
+}));
+
+const getComponentProps = (it) => {
+  switch (it.__component) {
+    case "section-cards.blog":
+      return { ...it, data: props.blogsData };
+    case "section-cards.case-studies":
+      return { ...it, data: props.caseStudyData };
+    case "section-cards.resources-blogs":
+      return { ...it, items: props.blogs };
+    case "section-cta.banner":
+      return { ...it, ...props.bannerData };
+    default:
+      return it;
+  }
+};
+</script>
