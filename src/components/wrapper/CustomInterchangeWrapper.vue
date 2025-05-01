2faf85c8e9a64a660f90d56e594c366476c51318
ktx-monil <monil.desai@kiara.tech>
2025-05-01 14:42:31 +0530

    fix: change css in Wrapper component of Interchange

--- src/components/wrapper/CustomInterchangeWrapper.vue
+++ src/components/wrapper/CustomInterchangeWrapper.vue
@@ -11,14 +11,13 @@ const props = defineProps({
 </script>
 
 <template>
-  <CustomSection sectionClass="py-0 sm:py-0 md:py-0 lg:py-0 xl:py-0">
-      <div v-for="(card, index) in interchangeCard" :key="index">
-        <CustomInterchange
-          :heading="card.heading"
-          :cardData="card.cardData"
-          :items="card.items"
-          :direction="card.direction || 'left'"
-        />
+  <div v-for="(card, index) in interchangeCard" :key="index"
+    :class="card.direction === 'left' ? 'bg-white' : 'bg-gray-100'">
+    <CustomSection sectionClass="py-0 sm:py-0 md:py-0 lg:py-0 xl:py-0">
+      <div>
+        <CustomInterchange :heading="card.heading" :cardData="card.cardData" :items="card.items"
+          :direction="card.direction || 'left'" />
       </div>
-      </CustomSection>  
+    </CustomSection>
+  </div>
 </template>
