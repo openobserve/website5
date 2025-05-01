1747d1dfb529ea38002e57bd3f4168df42ba3dd6
ktx-monil <monil.desai@kiara.tech>
2025-05-01 12:22:30 +0530

    fix: change color of tabsHeader

--- src/components/core/TabsHeader.vue
+++ src/components/core/TabsHeader.vue
@@ -3,7 +3,7 @@
     <div class="grid w-full max-w-3xl grid-cols-2 md:grid-cols-4 gap-2">
       <button v-for="tab in tabs" :key="tab.value" @click="$emit('update:activeTab', tab.value)"
         class="px-4 py-2 text-sm font-medium rounded-md transition-colors cursor-pointer" :class="{
-          'bg-white text-black shadow': activeTab === tab.value,
+          'bg-white text-primary-purple shadow': activeTab === tab.value,
           'bg-gray-100 text-gray-700 hover:bg-gray-200': activeTab !== tab.value
         }">
         {{ tab.label }}
