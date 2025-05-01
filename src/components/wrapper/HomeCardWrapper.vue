8a9d20177e6f92abd8b1516aae7845c6e7f34275
ktx-monil <monil.desai@kiara.tech>
2025-05-01 15:57:47 +0530

    fix: make required css and json content changes for show content

--- src/components/wrapper/HomeCardWrapper.vue
+++ src/components/wrapper/HomeCardWrapper.vue
@@ -1,29 +1,31 @@
-<script setup lang="ts">
-import CardWithShadowBorder from "../core/CardWithShadowBorder.vue";
-import CustomSection from "../core/CustomSection.vue";
+  <script setup lang="ts">
+  import CardWithShadowBorder from "../core/CardWithShadowBorder.vue";
+  import CustomSection from "../core/CustomSection.vue";
 
-const props = defineProps<{
-  data: Array<{
-    title: string;
-    description: string;
-    icon: string;
-    buttonText?: string;
-    buttonLink?: string;
-    theme?: string;
-    items?: string[];
-  }>;
-}>();
+  const props = defineProps<{
+    align?: string;
+    data: Array<{
+      title: string;
+      description: string;
+      icon: string;
+      buttonText?: string;
+      buttonLink?: string;
+      theme?: string;
+      items?: string[];
+    }>;
+  }>();
+  console.log(props.data);
 </script>
 
-<template>
-  <div class="bg-gray-50">
-    <CustomSection>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <CardWithShadowBorder v-for="(card, index) in props.data" :key="index" :title="card.title"
-          :description="card.description" :icon="card.icon" :buttonText="card.buttonText" :buttonLink="card.buttonLink"
-          :theme="card.theme" :items="card.items" />
+  <template>
+    <div class="bg-gray-50">
+      <CustomSection>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <CardWithShadowBorder v-for="(card, index) in props.data" :key="index" :title="card.title"
+            :description="card.description" :icon="card.icon" :buttonText="card.buttonText"
+            :buttonLink="card.buttonLink" :theme="card.theme" :items="card.items" :align="align || 'left'" />
 
-      </div>
-    </CustomSection>
-  </div>
-</template>
+        </div>
+      </CustomSection>
+    </div>
+  </template>
