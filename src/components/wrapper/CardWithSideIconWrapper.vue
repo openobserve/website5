79c48d4a923c739d08a59164032a5abefbbf62d6
ktx-monil <monil.desai@kiara.tech>
2025-05-01 18:08:03 +0530

    feat: add tabs in platform page for show different architechture content

--- src/components/wrapper/CardWithSideIconWrapper.vue
+++ src/components/wrapper/CardWithSideIconWrapper.vue
@@ -1,9 +1,10 @@
 <script setup>
+import { ref } from 'vue';
 import CustomSection from "../core/CustomSection.vue";
 import HeadingSection from "../core/HeadingSection.vue";
-import CardWithShadowBorder from "../core/CardWithShadowBorder.vue";
 import CardWithSideIcon from "../core/CardWithSideIcon.vue";
 import CustomButton from "../core/CustomButton.vue";
+import TabsHeader from "../core/TabsHeader.vue";
 
 const props = defineProps({
   title: {
@@ -27,6 +28,13 @@ const props = defineProps({
     required: true,
   },
 });
+const tabs = [
+  { label: 'Standard', value: 'standard' },
+  { label: 'Enterprise', value: 'enterprise' }
+]
+
+const activeTab = ref('standard')
+
 </script>
 
 <template>
@@ -46,16 +54,24 @@ const props = defineProps({
           </button>
         </div>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <!-- CardWithSideIcon -->
-        <CardWithSideIcon v-for="(card, index) in data" :key="index" :title="card.title" :description="card.description"
-          :icon="card.icon" :theme="card.theme" :items="card.items" />
-      </div>
-      <div class="text-center mt-6">
-        <CustomButton :buttonLink="btnValues?.buttonLink" :btn-class="btnValues?.theme" size="small" class="text-md inline-flex px-10 py-4"
-          :buttonText="btnValues?.buttonText" />
+
+      <!-- Centered Tabs -->
+      <div class="flex justify-center mt-6">
+        <div class="inline-flex">
+          <TabsHeader :tabs="tabs" :activeTab="activeTab" @update:activeTab="activeTab = $event" />
+        </div>
       </div>
 
+      <!-- Card rendering based on activeTab -->
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+        <CardWithSideIcon v-for="(card, index) in props.data[activeTab]" :key="index" :title="card.title"
+          :description="card.description" :icon="card.icon" :theme="card.theme" />
+      </div>
+
+      <div class="text-center mt-6">
+        <CustomButton :buttonLink="btnValues?.buttonLink" :btn-class="btnValues?.theme" size="small"
+          class="text-md inline-flex px-10 py-4" :buttonText="btnValues?.buttonText" />
+      </div>
     </div>
   </CustomSection>
 </template>
