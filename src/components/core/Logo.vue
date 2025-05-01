78855d1b6dd06b771e9d3416761675832da13d0a
krupa <krupa.jivani@kiara.tech>
2025-05-01 11:53:00 +0530

    feat: add new logo

--- src/components/core/Logo.vue
+++ src/components/core/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <a class="flex items-center w-40 md:w-52" href="/">
-    <img src="/img/logo.svg" alt="OpenObserve Logo" class="p-1 w-full h-full" />
+    <img src="/img/o2-logo.svg" alt="OpenObserve Logo" class="p-1 w-full h-full" />
   </a>
 </template>
