4c86e1eedef6971599660c3bb4d0aaa87c82ef8d
ktx-monil <monil.desai@kiara.tech>
2025-05-02 10:04:17 +0530

    feat: add heroSection of subPage and add content for it

--- src/components/ComponentCollectionMapper.vue
+++ src/components/ComponentCollectionMapper.vue
@@ -1,11 +1,7 @@
 <template>
   <div>
-    <component
-      v-for="(it, index) in data"
-      :key="it.id || `${it.__component}-${index}`"
-      :is="componentsMap[it.__component]"
-      v-bind="getComponentProps(it)"
-    />
+    <component v-for="(it, index) in data" :key="it.id || `${it.__component}-${index}`"
+      :is="componentsMap[it.__component]" v-bind="getComponentProps(it)" />
   </div>
 </template>
 
@@ -34,6 +30,7 @@ import CustomInterchangeWrapper from "@/components/wrapper/CustomInterchangeWrap
 import OurStory from "@/components/core/OurStory.vue";
 import OurCommitmentO2 from "@/components/core/OurCommitmentO2.vue";
 import OurPartners from "@/components/core/OurPartners.vue";
+import HeroSectionWithImage from "./herosection/HeroSectionWithImage.vue";
 const props = defineProps({
   data: { type: Array, required: true },
   blogsData: { type: Array, required: false },
@@ -66,6 +63,7 @@ const componentsMap = computed(() => ({
   "section-story.our-commitment": OurCommitmentO2,
   "section-hero.common-hero": CommonHeroSection,
   "section-story.our-partners": OurPartners,
+  "section-hero.platform-sub-hero": HeroSectionWithImage
 }));
 
 const getComponentProps = (it) => {
