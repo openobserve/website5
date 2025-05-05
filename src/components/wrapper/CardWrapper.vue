076f75c48861a39e7640df0e20e8ced71acdd4c9
ktx-sadhna <sadhna.singh@kiara.tech>
2025-05-05 17:02:47 +0530

    fix(cardwrapper): fix card wrapper css to render items in grid

--- src/components/wrapper/CardWrapper.vue
+++ src/components/wrapper/CardWrapper.vue
@@ -34,7 +34,7 @@ const props = defineProps({
         />
 
         <!-- Cards Section -->
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+        <div class="grid-container mt-12">
           <!-- CardWithShadowBorder -->
           <CardWithShadowBorder
             v-for="(card, index) in items"
@@ -46,7 +46,7 @@ const props = defineProps({
             :buttonLink="card.buttonLink"
             :theme="card.theme"
             :items="card.items"
-          />
+          class="grid-item" />
 
           <!-- CardWithSideIcon -->
           <!-- <CardWithSideIcon v-for="(card, index) in cardsData2" :key="index" :title="card.title"
@@ -56,3 +56,59 @@ const props = defineProps({
     </CustomSection>
   </div>
 </template>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(6, minmax(auto, 1fr));
+  /* Adjust the number of columns */
+  gap: 1.5rem;
+  justify-content: center;
+  /* Center the grid items */
+  grid-auto-flow: dense;
+  /* Make sure the last row fills gaps */
+}
+
+.grid-item {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  grid-column: span 2;
+}
+
+@media (min-width: 768px) and (max-width: 1023px) {
+  .grid-container {
+    grid-template-columns: repeat(4, minmax(auto, 1fr));
+    /* Adjust the number of columns */
+  }
+
+  .grid-item:nth-last-child(1):nth-child(odd) {
+    /* grid-column: 1 / -1; */
+    grid-column-end: -2;
+  }
+}
+
+@media (max-width: 767px) {
+  .grid-container {
+    grid-template-columns: repeat(1, minmax(auto, 1fr));
+    /* Adjust the number of columns */
+  }
+}
+
+@media (min-width: 1024px) {
+  /* Optional: Add a counter if you need to display item numbers */
+  .grid-item:last-child:nth-child(3n - 1) {
+    grid-column-end: -2;
+    /* Span across the last 2 columns */
+  }
+
+  .grid-item:nth-last-child(2):nth-child(3n + 1) {
+    grid-column-end: 4;
+    /* Span across the 4th column */
+  }
+
+  .grid-item:last-child:nth-child(3n - 2) {
+    grid-column-end: 5;
+    /* Span across the 5th column */
+  }
+}
+</style>
