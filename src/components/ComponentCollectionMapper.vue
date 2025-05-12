f104936df06280922e9aaf76819eef243f45ee96
krupa <krupa.jivani@kiara.tech>
2025-05-12 11:51:27 +0530

    feat: add new content for the platform landing page and update page architacture based on the new content

--- src/components/ComponentCollectionMapper.vue
+++ src/components/ComponentCollectionMapper.vue
@@ -47,6 +47,7 @@ import TOC from "./policies/TOC.vue";
 import SummaryWrapper from "./policies/SummaryWrapper.vue";
 import PoliciesHeader from "./policies/PoliciesHeader.vue";import DemoForm from "./forms/DemoForm.vue";
 import DemoWrapper from "./wrapper/DemoWrapper.vue";
+import PlatformKeyFeaturesWrapper from "./wrapper/PlatformKeyFeaturesWrapper.vue";
 
 const props = defineProps({
   data: { type: Array, required: true },
@@ -93,6 +94,7 @@ const componentsMap = computed(() => ({
   "section-contact.community-support": CommunitySupport,
   "section-policies.table-of-contents": TOC,
   "section-policies.summary": SummaryWrapper,
+  "section-key-features-benefits.key-features": PlatformKeyFeaturesWrapper
 }));
 
 const getComponentProps = (it) => {
