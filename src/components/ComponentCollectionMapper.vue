ef58a57f1050fb160989b1f0ec65bc81c14eea44
krupa <krupa.jivani@kiara.tech>
2025-05-12 17:43:00 +0530

    feat: add data for the all platform subpages

--- src/components/ComponentCollectionMapper.vue
+++ src/components/ComponentCollectionMapper.vue
@@ -53,6 +53,7 @@ import PoliciesHeader from "./policies/PoliciesHeader.vue";
 import DemoForm from "./forms/DemoForm.vue";
 import DemoWrapper from "./wrapper/DemoWrapper.vue";
 import PlatformKeyFeaturesWrapper from "./wrapper/PlatformKeyFeaturesWrapper.vue";
+import CommonFaqWrapper from "./faqs/CommonFaqWrapper.vue";
 
 const props = defineProps({
   data: { type: Array, required: true },
@@ -98,7 +99,8 @@ const componentsMap = computed(() => ({
   "section-contact.community-support": CommunitySupport,
   "section-policies.table-of-contents": TOC,
   "section-policies.summary": SummaryWrapper,
-  "section-key-features-benefits.key-features": PlatformKeyFeaturesWrapper
+  "section-key-features-benefits.key-features": PlatformKeyFeaturesWrapper,
+  "sections.faq": CommonFaqWrapper
 }));
 
 const getComponentProps = (it) => {
