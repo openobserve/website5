85781ac001edb39dfeab8afac3fbf5f4965a47ec
krupa <krupa.jivani@kiara.tech>
2025-05-12 14:34:07 +0530

    feat: update platform logs page content and update component accordingly

--- src/components/TabsComponent/CapabilityTabs.vue
+++ src/components/TabsComponent/CapabilityTabs.vue
@@ -1,50 +1,123 @@
 <template>
   <div ref="sectionRef" :class="background ? 'bg-gray-50' : ''">
     <CustomSection>
-        <HeadingSection :title="title" :description="description" align="CENTER" />
+      <HeadingSection
+        :title="title"
+        :description="description"
+        align="CENTER"
+      />
       <div class="w-full">
-        <div class="grid w-full grid-cols-2 md:grid-cols-5 h-auto p-0 bg-transparent gap-2">
-          <button v-for="tab in tabs" :key="tab.value" @click="setActiveTab(tab.value)" :class="[
-            'py-3 flex flex-col items-center gap-2 transition-colors rounded-lg cursor-pointer',
-            activeTab === tab.value ? 'bg-primary-purple text-white' : 'text-gray-700 hover:bg-gray-100'
-          ]">
-            <img :src="tab.icon" :alt="tab.title+'icon'" class="h-5 w-5" />
+        <div
+          class="grid w-full grid-cols-2 md:grid-cols-5 h-auto p-0 bg-transparent gap-2"
+        >
+          <button
+            v-for="tab in tabs"
+            :key="tab.value"
+            @click="setActiveTab(tab.value)"
+            :class="[
+              'py-3 flex flex-col items-center gap-2 transition-colors rounded-lg cursor-pointer',
+              activeTab === tab.value
+                ? 'bg-primary-purple text-white'
+                : 'text-gray-700 hover:bg-gray-100',
+            ]"
+          >
+            <img :src="tab.icon" :alt="tab.title + 'icon'" class="h-5 w-5" />
             <span>{{ tab.title }}</span>
           </button>
         </div>
 
         <div class="mt-8">
-          <div v-for="tab in tabs" :key="'content-' + tab.value" v-show="activeTab === tab.value">
-            <div class="bg-white rounded-lg shadow">
-              <div class="p-6">
+          <div
+            v-for="tab in tabs"
+            :key="'content-' + tab.value"
+            v-show="activeTab === tab.value"
+          >
+            <div
+              class="bg-white rounded-lg shadow p-6 flex flex-col md:flex-row items-center justify-between w-full gap-4"
+            >
+              <div class="w-full md:w-1/2">
                 <h2 class="text-2xl font-bold mb-2">{{ tab.card.title }}</h2>
-                <p class="text-gray-600 mb-6">{{ tab.card.description }}</p>
-                <div class="grid md:grid-cols-2 gap-6 items-center">
+                <p class="text-gray-600 mb-6" v-if="tab.card.description">
+                  {{ tab.card.description }}
+                </p>
+                <div class="">
+                  <ul class="space-y-2 mb-6">
+                    <li
+                      v-for="(feature, index) in tab.card.features"
+                      :key="index"
+                      class="flex flex-col justify-start space-y-2 items-start"
+                    >
+                      <h3
+                        class="text-lg font-semibold text-gray-800 mb-2"
+                        v-if="feature.title"
+                      >
+                        {{ feature.title }}
+                      </h3>
+                      <p class="text-gray-600 mb-2" v-if="feature.description">
+                        {{ feature.description }}
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="w-full md:w-1/2">
+                <div
+                  class="relative h-[300px] w-full rounded-lg overflow-hidden"
+                >
+                  <img
+                    :src="tab.card.image"
+                    :alt="tab.card.title"
+                    class="object-cover w-full h-full"
+                  />
+                </div>
+              </div>
+            </div>
+            <!-- <div class="grid md:grid-cols-2 gap-6 items-center">
                   <div>
                     <p class="mb-4">{{ tab.card.content }}</p>
                     <ul class="space-y-2 mb-6">
-                      <li v-for="(feature, index) in tab.card.features" :key="index" class="flex items-start">
-                        <span class="bg-primary-purple text-white rounded-full p-1 mr-2 mt-0.5">
-                          <svg class="h-3 w-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
-                            </path>
+                      <li
+                        v-for="(feature, index) in tab.card.features"
+                        :key="index"
+                        class="flex items-start"
+                      >
+                        <span
+                          class="bg-primary-purple text-white rounded-full p-1 mr-2 mt-0.5"
+                        >
+                          <svg
+                            class="h-3 w-3"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M5 13l4 4L19 7"
+                            ></path>
                           </svg>
                         </span>
                         <span>{{ feature }}</span>
                       </li>
                     </ul>
                     <button
-                      class="bg-primary-purple hover:bg-dark-purple text-white py-2 px-4 rounded transition-colors cursor-pointer">
+                      class="bg-primary-purple hover:bg-dark-purple text-white py-2 px-4 rounded transition-colors cursor-pointer"
+                    >
                       {{ tab.card.buttonText }}
                     </button>
                   </div>
-                  <div class="relative h-[300px] w-full rounded-lg overflow-hidden">
-                    <img :src="tab.card.image" :alt="tab.card.title" class="object-cover w-full h-full" />
+                  <div
+                    class="relative h-[300px] w-full rounded-lg overflow-hidden"
+                  >
+                    <img
+                      :src="tab.card.image"
+                      :alt="tab.card.title"
+                      class="object-cover w-full h-full"
+                    />
                   </div>
-                </div>
-              </div>
-            </div>
+                </div> -->
           </div>
         </div>
       </div>
@@ -53,78 +126,78 @@
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue'
-import HeadingSection from '../core/HeadingSection.vue'
-import CustomSection from '../core/CustomSection.vue'
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import HeadingSection from "../core/HeadingSection.vue";
+import CustomSection from "../core/CustomSection.vue";
 const props = defineProps({
   title: String,
   description: String,
   tabs: {
     type: Array,
-    required: true
+    required: true,
   },
   background: {
     type: Boolean,
-    required: false
-  }
-})
-const activeTab = ref('performance')
-const autoRotate = ref(true)
-const sectionRef = ref(null)
-let interval = null
+    required: false,
+  },
+});
+const activeTab = ref("performance");
+const autoRotate = ref(true);
+const sectionRef = ref(null);
+let interval = null;
 
 // Function to move to the next tab
 const rotateToNextTab = () => {
-  const currentIndex = tabs.findIndex(tab => tab.value === activeTab.value)
-  const nextIndex = (currentIndex + 1) % tabs.length
-  activeTab.value = tabs[nextIndex].value
-}
+  const currentIndex = tabs.findIndex((tab) => tab.value === activeTab.value);
+  const nextIndex = (currentIndex + 1) % tabs.length;
+  activeTab.value = tabs[nextIndex].value;
+};
 
 const setActiveTab = (tabValue) => {
-  activeTab.value = tabValue
+  activeTab.value = tabValue;
   if (autoRotate.value) {
-    autoRotate.value = false
+    autoRotate.value = false;
   }
-}
+};
 
 // Set up auto-rotation
 onMounted(() => {
   if (autoRotate.value) {
     interval = setInterval(() => {
-      rotateToNextTab()
-    }, 3000)
+      rotateToNextTab();
+    }, 3000);
   }
 
-  const sectionElement = sectionRef.value
+  const sectionElement = sectionRef.value;
   if (sectionElement) {
-    sectionElement.addEventListener('click', handleInteraction)
-    sectionElement.addEventListener('touchstart', handleInteraction)
+    sectionElement.addEventListener("click", handleInteraction);
+    sectionElement.addEventListener("touchstart", handleInteraction);
   }
-})
+});
 
 // Stop auto-rotation when user interacts with the section
 const handleInteraction = () => {
   if (autoRotate.value) {
-    autoRotate.value = false
+    autoRotate.value = false;
     if (interval) {
-      clearInterval(interval)
-      interval = null
+      clearInterval(interval);
+      interval = null;
     }
   }
-}
+};
 
 // Clean up
 onBeforeUnmount(() => {
   if (interval) {
-    clearInterval(interval)
+    clearInterval(interval);
   }
 
-  const sectionElement = sectionRef.value
+  const sectionElement = sectionRef.value;
   if (sectionElement) {
-    sectionElement.removeEventListener('click', handleInteraction)
-    sectionElement.removeEventListener('touchstart', handleInteraction)
+    sectionElement.removeEventListener("click", handleInteraction);
+    sectionElement.removeEventListener("touchstart", handleInteraction);
   }
-})
+});
 </script>
 
 <style scoped>
--- src/components/herosection/HeroSectionWithImage.vue
+++ src/components/herosection/HeroSectionWithImage.vue
@@ -4,60 +4,82 @@ import CustomImage from "../core/CustomImage.vue";
 import CustomSection from "../core/CustomSection.vue";
 
 const props = defineProps({
-    pageTag: {
-        type: String,
-        required: false,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    buttons: {
-        type: Array as () => Array<{ text: string; link: string; variant: string }>,
-        required: false,
-        default: () => [],
-    },
-    image: {
-        type: String,
-        required: true,
-    },
-    altText: {
-        type: String,
-        required: false,
-    }
+  pageTag: {
+    type: String,
+    required: false,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  altText: {
+    type: String,
+    required: false,
+  },
+  primaryButton: {
+    type: Object,
+    required: true,
+  },
+  secondaryButton: {
+    type: Object,
+    required: false,
+  },
 });
 </script>
 
 <template>
-    <div class="w-full hero-gradient">
-        <CustomSection>
-            <div class="flex flex-col lg:flex-row items-center gap-6">
-                <div
-                    class="w-full  flex-1 lg:w-1/2 mx-auto flex flex-col  items-center lg:items-start text-center lg:text-left space-y-5">
-                    <div
-                        class="w-fit flex items-center justify-center rounded-3xl bg-white/20 hover:bg-white/30 py-1 px-4">
-                        <h2 class="text-sm font-semibold text-white text-center capitalize">{{ pageTag }}</h2>
-                    </div>
-                    <h1 class="text-3xl lg:text-5xl font-bold text-white">
-                        {{ title }}
-                    </h1>
-                    <p class="text-white text-base md:text-xl">
-                        {{ description }}
-                    </p>
-                    <!-- Button -->
-                    <div v-if="buttons.length" class="flex flex-col sm:flex-row justify-center gap-4">
-                        <CustomButton v-for="(button, index) in buttons" :key="index" :variant="button.variant"
-                            size="medium" :buttonText="button.text" :buttonLink="button.link" />
-                    </div>
-                </div>
-                <div class="w-full lg:w-1/2 h-full">
-                    <CustomImage :image=image :altText=altText cssClass="rounded-lg" />
-                </div>
-            </div>
-        </CustomSection>
-    </div>
+  <div class="w-full hero-gradient">
+    <CustomSection>
+      <div class="flex flex-col lg:flex-row items-center gap-6">
+        <div
+          class="w-full flex-1 lg:w-1/2 mx-auto flex flex-col items-center lg:items-start text-center lg:text-left space-y-5"
+        >
+          <div
+            class="w-fit flex items-center justify-center rounded-3xl bg-white/20 hover:bg-white/30 py-1 px-4"
+          >
+            <h2 class="text-sm font-semibold text-white text-center capitalize">
+              {{ pageTag }}
+            </h2>
+          </div>
+          <h1 class="text-3xl lg:text-5xl font-bold text-white">
+            {{ title }}
+          </h1>
+          <p class="text-white text-base md:text-xl">
+            {{ description }}
+          </p>
+          <!-- Button -->
+          <div class="flex flex-col sm:flex-row justify-center gap-4">
+            <CustomButton
+              :variant="primaryButton.variant"
+              size="medium"
+              :buttonText="primaryButton.text"
+              :buttonLink="primaryButton.link"
+            />
+            <CustomButton
+              v-if="secondaryButton"
+              :variant="secondaryButton.variant"
+              size="medium"
+              :buttonText="secondaryButton.text"
+              :buttonLink="secondaryButton.link"
+            />
+          </div>
+        </div>
+        <div class="w-full lg:w-1/2 h-full">
+          <CustomImage
+            :image="image"
+            :altText="altText"
+            cssClass="rounded-lg"
+          />
+        </div>
+      </div>
+    </CustomSection>
+  </div>
 </template>
