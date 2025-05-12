4b650f35c32fc2a5bf80758ae618a4a53f2591cb
ktx-sadhna <sadhna.singh@kiara.tech>
2025-05-12 15:14:15 +0530

    Feature/add sitemap (#216)
    * feat(sitemap): add sitemap
    
    * feat(sitemap):  update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+import vue from "@astrojs/vue";
+import tailwindcss from "@tailwindcss/vite";
 
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: "http://localhost:3000", //change it with site url
+  integrations: [vue(), sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
