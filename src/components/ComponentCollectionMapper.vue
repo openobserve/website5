d963a730b730ce8cadb3e2cdd02d57980edec911
ktx-krupa <krupa.jivani@kiara.tech>
2025-05-13 10:50:04 +0530

    Revert "Feature/v3/update content for pages (#218)"
    This reverts commit c31ace8376588babfaf7ea45a2aa0293dc254b57.

--- src/components/ComponentCollectionMapper.vue
+++ src/components/ComponentCollectionMapper.vue
@@ -47,8 +47,6 @@ import TOC from "./policies/TOC.vue";
 import SummaryWrapper from "./policies/SummaryWrapper.vue";
 import PoliciesHeader from "./policies/PoliciesHeader.vue";import DemoForm from "./forms/DemoForm.vue";
 import DemoWrapper from "./wrapper/DemoWrapper.vue";
-import PlatformKeyFeaturesWrapper from "./wrapper/PlatformKeyFeaturesWrapper.vue";
-import CommonFaqWrapper from "./faqs/CommonFaqWrapper.vue";
 
 const props = defineProps({
   data: { type: Array, required: true },
@@ -95,8 +93,6 @@ const componentsMap = computed(() => ({
   "section-contact.community-support": CommunitySupport,
   "section-policies.table-of-contents": TOC,
   "section-policies.summary": SummaryWrapper,
-  "section-key-features-benefits.key-features": PlatformKeyFeaturesWrapper,
-  "sections.faq": CommonFaqWrapper
 }));
 
 const getComponentProps = (it) => {
