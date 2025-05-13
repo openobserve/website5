c107dd8c2a74a9fac688d8f745fe022116563d23
ktx-sadhna <sadhna.singh@kiara.tech>
2025-05-13 10:53:55 +0530

    fix(blogcard): render author image in blogcard

--- src/components/blog/BlogListing.vue
+++ src/components/blog/BlogListing.vue
@@ -2,6 +2,8 @@
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
     <div v-for="(item, index) in sectionData" :key="index">
       <BlogCard2 :blog="item" :type="type" :authors="getAuthorsForBlog(item)" />
+    <div v-for="(item, index) in sectionData" :key="index">
+      <BlogCard2 :blog="item" :type="type" :authors="getAuthorsForBlog(item)" />
     </div>
   </div>
 </template>
@@ -11,6 +13,7 @@ import type { Blog } from "@/types/blog";
 import BlogCard2 from "./BlogCard2.vue";
 import { fetchAuthorsMapFromBlogs } from "@/utils/blogAuthorHelper";
 import { ref, onMounted, toRaw } from "vue";
+
 const props = defineProps<{
   sectionData: Blog[];
   type: string;
