29b85c96797ffc89abffd394aa7d702e2a43e5e8
ktx-krupa <krupa.jivani@kiara.tech>
2025-05-13 10:36:34 +0530

    Feature/v3/update content for pages (#218)
    * feat: add new content for the platform landing page and update page architacture based on the new content
    
    * feat: update platform logs page content and update component accordingly
    
    * feat: add data for the all platform subpages

--- src/components/ComponentCollectionMapper.vue
+++ src/components/ComponentCollectionMapper.vue
@@ -47,6 +47,8 @@ import TOC from "./policies/TOC.vue";
 import SummaryWrapper from "./policies/SummaryWrapper.vue";
 import PoliciesHeader from "./policies/PoliciesHeader.vue";import DemoForm from "./forms/DemoForm.vue";
 import DemoWrapper from "./wrapper/DemoWrapper.vue";
+import PlatformKeyFeaturesWrapper from "./wrapper/PlatformKeyFeaturesWrapper.vue";
+import CommonFaqWrapper from "./faqs/CommonFaqWrapper.vue";
 
 const props = defineProps({
   data: { type: Array, required: true },
@@ -93,6 +95,8 @@ const componentsMap = computed(() => ({
   "section-contact.community-support": CommunitySupport,
   "section-policies.table-of-contents": TOC,
   "section-policies.summary": SummaryWrapper,
+  "section-key-features-benefits.key-features": PlatformKeyFeaturesWrapper,
+  "sections.faq": CommonFaqWrapper
 }));
 
 const getComponentProps = (it) => {
