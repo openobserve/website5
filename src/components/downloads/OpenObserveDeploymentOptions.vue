13ea42aa382e0a8fa17ae9ae23b76ddd37ed12fe
ktx-sadhna <sadhna.singh@kiara.tech>
2025-05-16 11:43:50 +0530

    fix(downolad): modify optioncard and deploymentoption component (#236)
    * fix(downolad): modify optioncard and deploymentoption component
    
    * fix(log): remove log statement

--- src/components/downloads/OpenObserveDeploymentOptions.vue
+++ src/components/downloads/OpenObserveDeploymentOptions.vue
@@ -1,7 +1,14 @@
 <template>
   <CustomSection sectionClass="max-w-4xl !pb-0 !pt-14">
-    <HeadingSection :title="title" :description="description" align="center" class="!mb-10"/>
-    <div class="w-full flex flex-col gap-4 items-center h-full justify-center mt-6 lg:px-12">
+    <HeadingSection
+      :title="title"
+      :description="description"
+      align="center"
+      class="!mb-10"
+    />
+    <div
+      class="w-full flex flex-col gap-4 items-center h-full justify-center mt-6 lg:px-12"
+    >
       <!-- <TabsHeader
         :tabs="tabs"
         :activeTab="activeTab"
@@ -9,29 +16,14 @@
         gridClass="grid w-full justify-center grid-cols-2 gap-2"
       />
       <OptionsCard :tabs="tabs" :activeTab="activeTab" /> -->
-        <!-- Main Tabs -->
-        <TabsHeader
+      <!-- Main Tabs -->
+      <TabsHeader
         :tabs="tabs"
         :activeTab="activeTab"
         @update:activeTab="activeTab = $event"
         gridClass="grid w-full justify-center grid-cols-2 gap-2"
       />
-
-      <!-- SubTabs logic -->
-      <template v-if="currentTab?.subTabs">
-        <TabsHeader
-          :tabs="currentTab.subTabs"
-          :activeTab="activeSubTab"
-          @update:activeTab="activeSubTab = $event"
-          gridClass="grid w-full justify-center grid-cols-2 gap-2"
-        />
-        <OptionsCard :tabs="currentTab.subTabs" :activeTab="activeSubTab" />
-      </template>
-
-      <!-- No SubTabs -->
-      <template v-else>
-        <OptionsCard :tabs="tabs" :activeTab="activeTab" />
-      </template>
+      <OptionsCard :tabs="tabs" :activeTab="activeTab" />
     </div>
   </CustomSection>
 </template>
@@ -60,21 +52,8 @@ const props = defineProps<{
   };
 }>();
 const activeTab = ref(props.tabs[0]?.value || "");
-const activeSubTab = ref("");
-
-// Get current active main tab object
-const currentTab = computed(() =>
-  props.tabs.find((tab) => tab.value === activeTab.value)
-);
 
 // Watch for change in main tab to reset subtab
-watch(
-  () => activeTab.value,
-  (newVal) => {
-    const subTabs = props.tabs.find((t) => t.value === newVal)?.subTabs || [];
-    activeSubTab.value = subTabs[0]?.value || "";
-  },
-  { immediate: true }
-)
+watch(() => activeTab.value, { immediate: true });
 </script>
 <style scoped></style>
