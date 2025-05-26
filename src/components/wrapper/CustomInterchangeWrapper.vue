9a2325dd91bf7ded5257a897652357ea73de951f
ktx-sadhna <sadhna.singh@kiara.tech>
2025-05-26 14:50:32 +0530

    Feature/v3/homepage animation (#279)
    * feat(animation): making slide component for animation
    
    * feat(custominterchangewrappper): add animation to custominterchange card to render one by one

--- src/components/wrapper/CustomInterchangeWrapper.vue
+++ src/components/wrapper/CustomInterchangeWrapper.vue
@@ -1,6 +1,8 @@
 <script setup>
 import CustomInterchange from "../core/CustomInterchange.vue";
 import CustomSection from "../core/CustomSection.vue";
+import { motion } from "motion-v";
+import { ref } from "vue";
 
 const props = defineProps({
   items: {
@@ -8,24 +10,48 @@ const props = defineProps({
     required: true,
   },
 });
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.3,
+      ease: "easeIn",
+      delay: 0.15,
+    },
+  },
+};
 </script>
 
 <template>
-  <!-- <CustomSection sectionClass=""> -->
-    <div class="flex flex-col">
-      <div v-for="(card, index) in items" :key="index" :class="index % 2 == 0 ? 'bg-gray-50' : ''">
-      <CustomSection>
-        <CustomInterchange
-          :heading="card.heading"
-          :cardData="card.cardData"
-          :items="card.items"
-          :direction="card.direction || 'left'"
-          :theme="card.theme"
-          class=""
-          :id="card.id"
-        />
+  <div class="flex flex-col">
+    <div>
+      <!-- Each card tracks its own visibility -->
+      <motion.div
+        :initial="'hidden'"
+        :whileInView="'visible'"
+        :variants="itemVariants"
+        :inViewOptions="{ once: true, amount: 0.1 }"
+        v-for="(card, index) in items"
+        :key="index"
+        :class="index % 2 === 0 ? 'bg-gray-50' : ''"
+      >
+        <CustomSection>
+          <CustomInterchange
+            :heading="card.heading"
+            :cardData="card.cardData"
+            :items="card.items"
+            :direction="card.direction || 'left'"
+            :theme="card.theme"
+            :id="card.id"
+          />
         </CustomSection>
-      </div>
+      </motion.div>
     </div>
-      <!-- </CustomSection>   -->
+  </div>
 </template>
