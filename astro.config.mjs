a38b713c8d1a182707ff564cd120e8fbecf5843f
ktx-sadhna <sadhna.singh@kiara.tech>
2025-05-28 15:19:43 +0530

    fix(config): remove commented code

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,10 +11,6 @@ export default defineConfig({
   site: "https://o2-staging-website.pages.dev", //change it with site url
   integrations: [vue(), sitemap()],
   vite: {
-    //  server: {
-    //   host: true,
-    //   allowedHosts: ['.trycloudflare.com']
-    // },
     plugins: [tailwindcss()],
   }
 });
