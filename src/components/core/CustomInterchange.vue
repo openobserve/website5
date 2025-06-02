2c6ef298c35b2521dbceafcbda57562989faaa5e
ktx-monil <monil.desai@kiara.tech>
2025-06-02 11:25:55 +0530

    fix: change homepage pLevel and props name in interchangeWrapper and it's component

--- src/components/core/CustomInterchange.vue
+++ src/components/core/CustomInterchange.vue
@@ -76,7 +76,7 @@ const listContainerVariant = {
 const listItemVariant = {
   hidden: {
     opacity: 0,
-      x: props.direction === "RIGHT" ? -20 : 20,
+    x: props.direction === "RIGHT" ? -20 : 20,
   },
   visible: {
     opacity: 1,
@@ -96,49 +96,32 @@ const cardVariant = computed(() => {
 
 <template>
   <div :class="['w-full flex flex-col', props.theme]">
-    <HeadingSection
-      v-if="heading"
-      :title="heading.title"
-      :description="heading.description"
-    />
+    <HeadingSection v-if="heading" :title="heading.title" :description="heading.description" />
 
-    <div
-      class="w-full flex flex-col lg:flex-row justify-center items-center gap-12 overflow-x-hidden"
-      :class="sectionClasses"
-    >
+    <div class="w-full flex flex-col lg:flex-row justify-center items-center gap-12 overflow-x-hidden"
+      :class="sectionClasses">
       <!-- client review Section -->
-      <motion.div
-        :initial="'hidden'"
-        :whileInView="'visible'"
-        :variants="cardVariant"
+      <motion.div :initial="'hidden'" :whileInView="'visible'" :variants="cardVariant"
         :inViewOptions="{ once: true, amount: 0.3 }"
-        class="h-auto rounded-lg border border-card bg-white shadow-sm p-6 xl:p-10 z-10 w-full lg:w-1/2"
-      >
+        class="h-auto rounded-lg border border-card bg-white shadow-sm p-6 xl:p-10 z-10 w-full lg:w-1/2">
         <div class="flex flex-col items-start gap-4">
           <div class="flex flex-row items-start gap-3">
             <!-- Icon -->
-            <div
-              class="bg-black/5 text-color-primary-blue rounded-md p-4 shrink-0 mt-1"
-            >
+            <div class="bg-black/5 text-color-primary-blue rounded-md p-4 shrink-0 mt-1">
               <Quote class="w-6 h-6" />
             </div>
 
             <!-- Quote + Author Info -->
             <div class="flex flex-col">
-              <p
-                class="text-gray-600 italic text-base lg:text-lg font-medium mb-4"
-              >
+              <p class="text-gray-600 italic text-base lg:text-lg font-medium mb-4">
                 "{{ cardData?.quote }}"
               </p>
             </div>
             <!-- Author image and name,role,company -->
           </div>
           <div class="flex flex-row items-start gap-3">
-            <CustomImage
-              :src="cardData?.avatar"
-              class="w-12 h-12 rounded-full shrink-0"
-              :altText="cardData?.name"
-            />
+            <CustomImage :src="cardData?.image?.url" class="w-12 h-12 rounded-full shrink-0"
+              :altText="cardData?.image?.alternativeText || 'Author Image'" />
             <div>
               <p class="font-semibold text-base text-gray-900">
                 {{ cardData?.name }}
@@ -150,28 +133,14 @@ const cardVariant = computed(() => {
       </motion.div>
 
       <!-- Feature List -->
-      <motion.div
-        class="w-full lg:w-1/2 z-10 flex flex-col space-y-6"
-        :initial="'hidden'"
-        :whileInView="'visible'"
-        :variants="listContainerVariant"
-        :inViewOptions="{ once: true, amount: 0.3 }"
-      >
+      <motion.div class="w-full lg:w-1/2 z-10 flex flex-col space-y-6" :initial="'hidden'" :whileInView="'visible'"
+        :variants="listContainerVariant" :inViewOptions="{ once: true, amount: 0.3 }">
         <div v-if="items?.length" class="flex flex-col space-y-6">
-          <motion.div
-            v-for="(item, index) in items"
-            :key="index"
-            class="flex items-start"
-            :variants="listItemVariant"
-          >
+          <motion.div v-for="(item, index) in items" :key="index" class="flex items-start" :variants="listItemVariant">
             <div class="space-y-1">
               <div class="flex items-center">
-                <Check
-                  class="h-5 w-5 mr-2 bg-card rounded-full p-1 text-color-primary-blue"
-                />
-                <h3
-                  class="font-inter font-semibold text-sm md:text-base lg:text-lg text-black"
-                >
+                <Check class="h-5 w-5 mr-2 bg-card rounded-full p-1 text-color-primary-blue" />
+                <h3 class="font-inter font-semibold text-sm md:text-base lg:text-lg text-black">
                   {{ item.title }}
                 </h3>
               </div>
