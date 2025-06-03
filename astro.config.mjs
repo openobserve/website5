04df5d04f4bf13b66beab07146f384d71357abb0
ktx-sadhna <sadhna.singh@kiara.tech>
2025-06-03 15:29:46 +0530

    update(config): update config file site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://o2-staging-website.pages.dev", //change it with site url
+  site: "https://website-staging.zinc.dev/", //change it with site url
   integrations: [vue(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
