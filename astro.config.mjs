15cc34d4d9df6fa13ceabfc110615d65422cb48c
ktx-sadhna <sadhna.singh@kiara.tech>
2025-06-03 15:57:32 +0530

    update(config): update config file site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
 import vue from "@astrojs/vue";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
- const baseUrl = import.meta.env.PUBLIC_APP_BASE_URL;
+const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
+const baseUrl = env.PUBLIC_APP_BASE_URL 
 // https://astro.build/config
 export default defineConfig({
   site: baseUrl, //change it with site url
