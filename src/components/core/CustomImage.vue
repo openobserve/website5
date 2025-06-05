0bda46eaede9c4d557bd61bf4c5b999c18b8b26a
krupa <krupa.jivani@kiara.tech>
2025-06-05 17:18:08 +0530

    fix: work on the format images

--- src/components/core/CustomImage.vue
+++ src/components/core/CustomImage.vue
@@ -1,5 +1,4 @@
 <script setup>
-
 const props = defineProps({
   image: {
     type: [Object, String],
@@ -16,18 +15,45 @@ const props = defineProps({
     required: false,
     default: "",
   },
-  loading :{
+  loading: {
     type: String,
     required: false,
     default: "lazy",
-  }
+  },
 });
 
+// Return the correct URL from string or object
+function getImageUrl(source, size = null) {
+  if (!source) return "/img/default_project_image.png";
 
+  if (typeof source === "string") return source;
+
+  if (size && source.formats?.[size]?.url) {
+    return source.formats[size].url;
+  }
+
+  // if (source.url) return source.url;
+
+  return "/img/default_project_image.png";
+}
 </script>
 
 <template>
-  <img :src="image?.url || image || '/img/default_project_image.png'" :alt="altText || 'Default Image'"
-    :class="cssClass" :loading="loading" />
+  <img
+  :src="getImageUrl(image, 'medium')"
+  :srcset="`
+    ${getImageUrl(image, 'thumbnail')} 245w,
+    ${getImageUrl(image, 'small')} 500w,
+    ${getImageUrl(image, 'medium')} 750w,
+    ${getImageUrl(image, 'large')} 1000w,
+    ${getImageUrl(image)} 3024w
+  `"
+  sizes="(max-width: 480px) 90vw,
+         (max-width: 768px) 100vw,
+         (max-width: 1024px) 750px,
+         1000px"
+  :alt="altText || 'Image'"
+  :loading="loading"
+  :class="cssClass"
+/>
 </template>
-
