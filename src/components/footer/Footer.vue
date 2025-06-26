f1fdcb2648891e142bf62c0f4c78825597356a76
ktx-riya <riya.kukadiya@kiara.tech>
2025-06-26 12:06:43 +0530

    Add the script in the footer.

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -49,6 +49,17 @@ const socialIcon = (name) => {
   };
   return icons[name.toLowerCase()];
 };
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.innerHTML = `
+    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+    vgo('setAccount', '69568602');
+    vgo('setTrackByDefault', true);
+    vgo('process');
+  `;
+  document.body.appendChild(script);
+});
 </script>
 <template>
   <footer class="relative bg-primary-black w-full">
